02d8869eb6e371e8e02b41c51b1407727a914b10
muNeves3 <murilogrilosn@gmail.com>
2020-12-23 17:23:27 -0300

    customers and products created

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -21,21 +21,33 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = await this.ormRepository.create({ name, price, quantity });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const productByName = await this.ormRepository.findOne({ where: { name } });
+
+    return productByName;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const productsById = products.map(product => product.id);
+
+    const productsInProductsById = await this.ormRepository.find({
+      where: { id: In(productsById) },
+    });
+
+    return productsInProductsById;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    return this.ormRepository.save(products);
   }
 }
 
